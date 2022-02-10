81473577be58d82329869a74518fd0092baea860
elogeek <elodiechristin@gmail.com>
2022-02-10 12:31:33 +0100

    start defi two in symfony

--- src/EventSubscriber/KernelEventSubscriber.php
+++ src/EventSubscriber/KernelEventSubscriber.php
@@ -2,15 +2,24 @@
 
 namespace App\EventSubscriber;
 
+use App\Kernel;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Event\FinishRequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
  * Class KernelEventSubscriber
  */
 class KernelEventSubscriber implements EventSubscriberInterface {
+
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger) {
+        $this->logger = $logger;
+    }
 
     /**
      * Manage the events
@@ -25,6 +34,11 @@ class KernelEventSubscriber implements EventSubscriberInterface {
                 ['displayKernelException', 255],
                 ['onKernelException', 254],
                 ['logKernelException', 1]
+            ],
+
+            // Check that the request is finished  (Event kernel.finish_request)
+            KernelEvents::FINISH_REQUEST => [
+                ['logIpAdress',0],
             ]
         ];
     }
@@ -74,6 +88,18 @@ class KernelEventSubscriber implements EventSubscriberInterface {
      */
     public function logKernelException(ExceptionEvent $event) {
         $message = $event->getThrowable()->getMessage();
-        file_put_contents( __DIR__ . '/../var/log/test_log.log', $message, FILE_APPEND);
+        file_put_contents( $_SERVER['DOCUMENT_ROOT']. '/var/log/test_log.log', $message, FILE_APPEND);
     }
+
+    /**
+     * Log client ip address in the file dev.log
+     */
+    public function logIpAdress(FinishRequestEvent $event) {
+        if(!$event->isMainRequest()) {
+            return;
+        }
+        $addressIp = $event->getRequest()->getClientIp();
+        $this->logger->info('Request finished {"kernel.finish_request::logIpAddress()}', ['Request from'=>$addressIp]);
+    }
+
 }
